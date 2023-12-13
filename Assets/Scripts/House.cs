9fb063ce0ad43aa386143358949f83b7e6d9fa4b
SmolinIV <Smolin1992@bk.ru>
2023-12-13 19:06:36 +0500

    DONE with Coroutine!:)

--- Assets/Scripts/House.cs
+++ Assets/Scripts/House.cs
@@ -1,17 +1,17 @@
 using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class House : MonoBehaviour
 {
-    [SerializeField, Range(0.05f, 2f)] private float _volumeChangingSpeed;
+    [SerializeField, Range(0.2f, 2f)] private float _volumeChangingSpeed;
 
     private AudioSource _alarm;
+    private Coroutine _volumeChange;
 
-    private bool _isThiefInHome;
     private float _maxVolume;
     private float _minVolume;
+    private bool _isThiefAtHome;
+    private bool _isAlarmPlay;
 
     private void Start()
     {
@@ -19,14 +19,18 @@ public class House : MonoBehaviour
         _alarm.volume = 0;
         _maxVolume = 1.0f;
         _minVolume = 0f;
+
+        _isAlarmPlay = false;
     }
+
+    private void OnDestroy() => StopCoroutine(_volumeChange);
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.TryGetComponent(out Thief thief))
         {
-            _alarm.Play();
-            _isThiefInHome = true;
+            _isThiefAtHome = true;
+            _volumeChange = StartCoroutine(IncreaseAlarmVolume());
         }
     }
 
@@ -34,23 +38,43 @@ public class House : MonoBehaviour
     {
         if (collision.TryGetComponent(out Thief thief))
         {
-            _isThiefInHome = false;
+            _isThiefAtHome = false;
+            _volumeChange = StartCoroutine(DecreaseAlarmVolume());
         }
     }
 
-    private void Update()
+    private IEnumerator IncreaseAlarmVolume()
     {
-        if (_isThiefInHome)
+        if (!_isAlarmPlay)
         {
-            if (_alarm.volume < _maxVolume)
-                _alarm.volume += Time.deltaTime * _volumeChangingSpeed;
-        }      
-        else
-        {
-            if (_alarm.volume > _minVolume)
-                _alarm.volume -= Time.deltaTime * _volumeChangingSpeed;
-            else
-                _alarm.Stop();
+            _alarm.Play();
+            _isAlarmPlay = true;
         }
+
+        while (_alarm.volume < _maxVolume)
+        {
+            if (!_isThiefAtHome)
+                yield break;
+
+            _alarm.volume += Time.deltaTime * _volumeChangingSpeed;
+
+            yield return null;
+        }
+    }
+
+    private IEnumerator DecreaseAlarmVolume()
+    { 
+        while (_alarm.volume > _minVolume)
+        {
+            if (_isThiefAtHome)
+                yield break;
+
+            _alarm.volume -= Time.deltaTime * _volumeChangingSpeed;
+
+            yield return null;
+        }
+
+        _alarm.Stop();
+        _isAlarmPlay = false;
     }
 }
